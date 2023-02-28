eae2ff9eb312a517bf60544e920c003cdd201a85
AhsanATJ <Ahsanjr911@gmail.com>
2023-02-28 23:38:48 +0500

    firdy vommiy

--- Reels/ViewController.swift
+++ Reels/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var EmailText: UITextField!
+    
+    @IBOutlet weak var PasswordText: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var SignUpBtn: UIButton!
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
